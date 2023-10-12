b63f01136a0b31a102b4d521eb17b4502bbfb283
barrosohub <barrosofilho.ti@gmail.com>
2023-10-12 11:58:28 -0300

    ✨ feat: Update Tailwind configuration and remove unnecessary code
    - Removed unused CSS variables and media queries in the `globals.css` file.
    - Removed unused imports and JSX elements in the `page.tsx` file.
    - Added new color options for buttons and various elements in the `tailwind.config.ts` file.
    
    This commit improves the code organization and removes unnecessary code, allowing for a more streamlined and efficient application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "button-codante":
+                "linear-gradient(180deg, #E4E4E4 0%, #F7F7F7 100%)",
+                "invalid-codante": "#BF0E0E",
+                "valid-codante": "#1DB954",
+                "accent-codante": "#4E9590",
+                "brackets-codante": "#F2CAB8",
+                "gray-codante": "#BFBFBF",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
